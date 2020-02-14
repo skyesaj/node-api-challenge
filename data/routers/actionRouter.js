f5b7403ace9fb41b01554c40bcab566951494419
skye <skyesajous@gmail.com>
2020-02-14 13:43:41 -0700

    make sure

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -1,4 +1,5 @@
 const actions = require("../helpers/actionModel");
+// const projects = require("../routers/projectRouter");
 const express = require("express");
 const router = express.Router();
 
@@ -26,6 +27,40 @@ router.get("/:id", validateActionId, (req, res) => {
     });
 });
 
+router.delete("/:id", validateActionId, (req, res) => {
+  const { id } = req.params;
+  actions
+    .get(id)
+    .then(deleted => {
+      projects
+        .remove(deleted.id)
+        .then(deletes => {
+          // console.log(deleted);
+          res.status(200).json(deleted);
+        })
+        .catch(error => {
+          res.status(404).json({ error: error.message });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.put("/:id", validateActionId, validateAction, (req, res) => {
+  // do your magic!
+  const { id } = req.params;
+  const aUpdate = req.body;
+  actions
+    .update(id, aUpdate)
+    .then(updates => {
+      res.status(200).json(updates);
+    })
+    .catch(error => {
+      res.status(404).json({ error: error.message });
+    });
+});
+
 // middleware
 function validateActionId(req, res, next) {
   const { id } = req.params;
@@ -41,6 +76,18 @@ function validateActionId(req, res, next) {
     .catch(error => {
       res.status(500).json({ error: error.message });
     });
+}
+
+function validateAction(req, res, next) {
+  // do your magic!
+  const aData = req.body;
+
+  if ((aData.notes, data.description)) {
+    next();
+  } else {
+    // req.text = text;
+    res.status(404).json({ message: error.message });
+  }
 }
 
 module.exports = router;
